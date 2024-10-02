a44f65b2b63f3a0fe1fde9ab3efe3209ab730776
Emrysil <ctzszty@gmail.com>
2024-10-02 22:26:04 +0800

    Add more test cases for Header and ProductDetails components

--- client/src/components/Header.test.js
+++ client/src/components/Header.test.js
@@ -3,6 +3,7 @@ import { screen, render, fireEvent, waitFor } from "@testing-library/react";
 import Header from "./Header";
 import "@testing-library/jest-dom/extend-expect";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
+import useCategory from "../hooks/useCategory";
 jest.mock("../context/auth", () => ({
   useAuth: jest.fn(() => [null, jest.fn()]), // Mock useAuth hook to return null state and a mock function for setAuth
 }));
@@ -16,14 +17,12 @@ jest.mock("../context/search", () => ({
 }));
 const HOME_LIVING = "home and living";
 const FOOD_BEVERAGE = "food and beverages";
-jest.mock("../../src/hooks/useCategory", () => {
-  return () => {
-    return [
-      { name: HOME_LIVING, slug: HOME_LIVING.split(" ").join("-") },
-      { name: FOOD_BEVERAGE, slug: FOOD_BEVERAGE.split(" ").join("-") },
-    ];
-  };
-});
+jest.mock("../../src/hooks/useCategory", () =>
+  jest.fn(() => [
+    { name: HOME_LIVING, slug: HOME_LIVING.split(" ").join("-") },
+    { name: FOOD_BEVERAGE, slug: FOOD_BEVERAGE.split(" ").join("-") },
+  ])
+);
 describe("test Header component", () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -66,6 +65,186 @@ describe("test Header component", () => {
     const foodAndBeverage = screen.getByText(FOOD_BEVERAGE);
     expect(foodAndBeverage).toBeInTheDocument();
   });
+
+  it.failing(
+    "dropdown list does not display corrupted category items (missing slug)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        { name: "__PLACEHOLDER__" },
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", { href: "category/undefined" })
+      ).not.toBeInTheDocument();
+    }
+  );
+  it.failing(
+    "dropdown list does not display corrupted category items (missing name)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        { slug: "placeholder-slug-1" },
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug-2",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", { href: "/category/placeholder-slug-1" })
+      ).not.toBeInTheDocument();
+    }
+  );
+  it.failing(
+    "dropdown list does not display corrupted category items (empty name)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        { name: "", slug: "placeholder-slug-1" },
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug-2",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", { href: "/category/placeholder-slug-1" })
+      ).not.toBeInTheDocument();
+    }
+  );
+  it.failing(
+    "dropdown list does not display corrupted category items (empty slug)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        { name: "__PLACEHOLDER__", slug: "" },
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug-2",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", { href: "/category/" })
+      ).not.toBeInTheDocument();
+    }
+  );
+  it.failing(
+    "dropdown list does not display corrupted category items (empty slug, missing name)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        { slug: "" },
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug-2",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", { href: "/category/" })
+      ).not.toBeInTheDocument();
+    }
+  );
+  it.failing(
+    "dropdown list does not display corrupted category items (empty name, missing slug)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        { name: "" },
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug-2",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", { href: "/category/undefined" })
+      ).not.toBeInTheDocument();
+    }
+  );
+
+  it.failing(
+    "dropdown list does not display corrupted category items (empty name and slug)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        { name: "", slug: "" },
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug-2",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", { href: "/category/" })
+      ).not.toBeInTheDocument();
+    }
+  );
+  it.failing(
+    "dropdown list does not display empty category items (missing name and slug)",
+    async () => {
+      useCategory.mockImplementationOnce(() => [
+        {},
+        {
+          name: "__PLACEHOLDER__",
+          slug: "placeholder-slug",
+        },
+      ]);
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      expect(
+        screen.queryByRole("link", {
+          href: "/category/undefined",
+        })
+      ).not.toBeInTheDocument();
+    }
+  );
   it("dropdown list all categories item redirects correctly", async () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
